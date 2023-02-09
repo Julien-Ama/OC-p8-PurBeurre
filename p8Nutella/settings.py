fbc8a99cfd49c170c26d435d12978cc169066752
Julien-Ama <inglesejulien@hotmail.fr>
2023-02-09 14:57:45 +0100

    update allowed_host

--- p8Nutella/settings.py
+++ p8Nutella/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://p8-pbeurre.herokuapp.com/',"127.0.0.1"]
+ALLOWED_HOSTS = ['https://p8-pbeurre.herokuapp.com',"127.0.0.1"]
 
 # Application definition
 
