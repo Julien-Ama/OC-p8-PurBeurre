8aad140bd4943750841499ed7b59816ea3778d4e
Julien-Ama <inglesejulien@hotmail.fr>
2023-02-16 16:05:58 +0100

    update

--- p8Nutella/settings.py
+++ p8Nutella/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4'
         },
