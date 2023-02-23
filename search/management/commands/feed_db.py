f3e6681be86956cda7a44a870df035153c5927f0
Julien-Ama <inglesejulien@hotmail.fr>
2023-02-23 12:56:49 +0100

    test print

--- search/management/commands/feed_db.py
+++ search/management/commands/feed_db.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
         # Launches import of data from API
         print("Data importation from API")
         myapi = Api()
-        products = myapi.products
+        products = myapi.avoid_empty()
         print(products) # test products -------------------------------------------------
 
         if products is not None:
