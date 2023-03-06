d118b85b2856a91343bd647f7e407d67a1ba8b39
Julien-Ama <inglesejulien@hotmail.fr>
2023-03-06 16:53:57 +0100

    setting

--- p8Nutella/settings.py
+++ p8Nutella/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "search",
     "users",
     "crispy_forms",
-    # 'crispy_bootstrap5',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -149,7 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 LOGIN_REDIRECT_URL = "search:home"
-LOGIN_URL = "users:login"
+LOGIN_URL = "login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
