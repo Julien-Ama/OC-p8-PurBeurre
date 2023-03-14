462b186881635a5ad654985d48f3fa6a2af47b5d
Julien-Ama <inglesejulien@hotmail.fr>
2023-03-14 18:00:36 +0100

    staticfiles

--- p8Nutella/settings.py
+++ p8Nutella/settings.py
@@ -145,9 +145,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 LOGIN_REDIRECT_URL = "search:home"
 LOGIN_URL = "login"
